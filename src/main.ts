9893c289e6e4e296f6b6b59894c6708d913c95e3
I'm Yong Tack <aldjzk@naver.com>
2025-01-25 17:35:47 +0900

    #5 Swagger add

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
 
   const config = new DocumentBuilder()
     .setTitle('Cats example')
@@ -17,6 +16,7 @@ async function bootstrap() {
 
   // Correct way to set the Swagger UI route to '/api/docs'
   SwaggerModule.setup('api/docs', app, documentFactory);
+await app.listen(3000);
 
   console.log(`Swagger UI is available at /api/docs`);
 }
